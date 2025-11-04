3d0e7b61731876d967976b4eec88e86b50d45bb3
parnia-alipour <parnianor444@gmail.com>
2025-11-04 13:22:14 +0330

    new file2

---
+++ user.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+double ln,n
+
+cout<<'Please enter your name:';
+
+cin>>n;
+
+cout<<'Please enter your last_name:';
+
+cin>>ln
+
+
+cout<<n , ln;
+return 0;
+
+}
