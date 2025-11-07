6c085bf435bcab5f7c67b35329eca887745b2775
parnia <parnianor444@gmail.com>
2025-11-07 20:23:23 +0330

    add new file

---
+++ Mathematics.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main(){
+int num1,num2;
+
+cout<<"Please enter num:";
+cin>>num1;
+
+cout<<"Please enter num2:";
+cin>>num2;
+
+
+cout<<"Multiplication :"<<num1*num2<<endl;
+cout<<"Minus : "<<num1-num2<<endl;
+cout<<"Division :"<<num1/num2<<endl;
+cout<<"Modulus : "<<num1%num2<<endl;
+cout<<"addition : "<<num1+num2<<endl;
+
+}
