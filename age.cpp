998187437573b0fcd610731999ad24103c21051c
parnia <parnianor444@gmail.com>
2025-11-12 04:34:28 +0330

    new code

---
+++ age.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a,b;
+    cout<<"Enter this year: ";
+    cin>>a;
+    cout<<"Enter your birthday day years:";
+    cin>>b;
+    cout<<"your age"<<a-b;
+    return 0;
+}
