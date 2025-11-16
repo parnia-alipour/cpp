3d4818ad70141a485abbf08a35fabce027c46d06
parnia <parnianor444@gmail.com>
2025-11-17 02:57:37 +0330

    new code

---
+++ sqrt.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main() {
+
+    int a,sq;
+    cout<<"enter your num:";
+    cin>>a;
+    sq=sqrt(a);
+    cout<<sq;
+    return 0;
+}
