0214ca306ed6059db29a0fad65b6caf611ac8636
parnia <parnianor444@gmail.com>
2025-11-17 03:32:54 +0330

    new file and new code

---
+++ sample.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int num=0;
+    cout<<"enter your num:";
+    cin>>num;
+    num+=2;
+    cout<<num<<endl;
+    num-=2;
+    cout<<num<<endl;
+    num*=2;
+    cout<<num<<endl;
+    num/=4;
+    cout<<num<<endl;
+
+
+return 0;
+
+}
