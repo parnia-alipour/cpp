92b15a3bdf368b2db05e7673c84b06647c4f39cb
parnia <parnianor444@gmail.com>
2025-11-18 09:16:49 +0330

    new code

---
+++ try3.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+void fun(int n);
+int main() {
+    int n;
+    cout<<"Enter :";
+    cin>>n;
+    fun(n);
+    return 0;
+}
+    void fun(int n) {
+        n++;
+
+    }
