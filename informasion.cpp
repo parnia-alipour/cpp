703bac169cc1a9ea486ae6c9b74763ab692ff4ed
parnia <parnianor444@gmail.com>
2025-11-21 06:32:47 +0330

    new code

---
+++ informasion.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int user(int a,char b, char c) {
+    cout<<"enter your age"<<endl;
+    cin>>a;
+    cout<<"enter your name"<<endl;
+    cin>>b;
+    cout<<"enter your last_name"<<endl;
+    cin>>c;
+    cout<<user;
+}
