3bd178e762ac9836cabd133f528ff9b9307a0a35
parnia <parnianor444@gmail.com>
2025-12-06 14:10:49 +0330

    add new file

---
+++ try10.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(){
+int x,sum=0,i;
+    for (i=1;i<=5;i++){
+        cout<<"enter x:";
+        cin>>x;
+        sum+=x;
+
+        cout<<"sum="<<sum<<endl;
+
+    }
+
+}
