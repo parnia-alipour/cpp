3b3474d53030a0b831f9db0d84b3ea1c8be861ba
parnia <parnianor444@gmail.com>
2025-12-06 13:57:04 +0330

    add new file

---
+++ try8.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    float r,pi=3.14,c,m;
+    cout<<"enter :"<<endl;
+     cin>>r;
+     c=2*pi*r;
+     cout<<"mohit:"<<c<<endl;
+     //cout<<"enter 2:"<<endl;
+     //cin>>r;
+     m=pi*r*r;
+    cout<<"mahaht:"<<m<<endl;
+     return 0;
+}
