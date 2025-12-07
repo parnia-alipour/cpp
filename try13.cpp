cff1e9e48463afed62526704566ba543a3182251
parnia <parnianor444@gmail.com>
2025-12-07 12:30:44 +0330

    add new code and new file

---
+++ try13.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a[10]={7,2,5,3,15,8,9,18,23,1},i,x,flag=0;
+    cout<<"enter x:";
+    cin>>x;
+    int n=10;
+    for (i=0;i<n;i++) {
+        cout<<"\na[ "<<i<<"]="<<a[i];
+    }
+    for(i=n-1;i>=0;i--) {
+    }
+}
