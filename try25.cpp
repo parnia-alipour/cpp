d674d1de5a82d5d1cad21363c0019bb9909b2ea8
parnia <parnianor444@gmail.com>
2025-12-28 00:03:36 +0330

    new file

--- try25.cpp
+++ try25.cpp
@@ -1,3 +1,37 @@
-//
-// Created by LOQ on 12/27/2025.
-//
+#include <iostream>
+using namespace std;
+int main() {
+    int day;
+    cout<<"enter:"<<endl;
+    cin>>day;
+    switch (day) {
+        case 0:
+            cout<<"Sunday"<<endl;
+            break;
+
+        case 1:
+            cout<<"Saturday"<<endl;
+            break;
+        case 2:
+            cout<<"monday"<<endl;
+            break;
+        case 3:
+            cout<<"Tuesday"<<endl;
+            break;
+        case 4:
+            cout<<"Wednesday"<<endl;
+            break;
+        case 5:
+            cout<<"Thursday"<<endl;
+            break;
+        case 6:
+            cout<<"Friday"<<endl;
+            break;
+        default:
+            cout<<"Error!!!!!!!!! please enter 1__7:";
+
+    }
+    return 0;
+
+
+}
