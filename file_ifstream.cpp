40000a06fe5b6f1ffa35f3d5cbad20216a1c849c
parnia <parnianor444@gmail.com>
2025-12-29 03:40:35 +0330

    new file

--- file_ifstream.cpp
+++ file_ifstream.cpp
@@ -1,3 +1,24 @@
-//
-// Created by LOQ on 12/28/2025.
-//
+#include <iostream>
+#include <fstream>
+using namespace std;
+int main() {
+    ifstream f2 ("phone.txt");
+    long number;
+    string name,seartchname;
+    bool find=false;
+    cout<<"Enter your name: : ";
+    cin>>seartchname;
+    while (f2>>name>>number) {
+        f2>>name;
+        if (seartchname==name) {
+            cout<<name<<' '<<number<<endl;
+            find=true;
+        }
+    }
+    if (!find) {
+        cout<<"Not Found"<<endl;
+    }
+        return 0;
+
+
+}
